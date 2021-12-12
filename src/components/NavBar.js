c059c685986607a9a4de4a8e66bd024271a30a6d
chiaratinio <90952721+chiaratinio@users.noreply.github.com>
2021-12-12 18:25:10 +0800

    Updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,9 +6,9 @@ const NavBar = () => {
                       <a class="navbar-brand" href="/">PANDA 🐼</a>
                       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                          <a class="nav-link" href="/">Home</a>
-                          <a class="nav-link" href="/employees">Employees</a>
-                          <a class="nav-link" href="/add">Add Employee</a>
+                          <a class="nav-link" href="/myfirstreact">Home</a>
+                          <a class="nav-link" href="/myfirstreact/employees">Employees</a>
+                          <a class="nav-link" href="/myfirstreact/add">Add Employee</a>
                           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                         </div>
                       </div>
