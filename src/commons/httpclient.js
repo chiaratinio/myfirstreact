329b8b3a2ec3e3f27388396868c91046c940e21a
chiaratinio <90952721+chiaratinio@users.noreply.github.com>
2021-12-15 10:38:15 +0800

    Update httpclient.js

--- src/commons/httpclient.js
+++ src/commons/httpclient.js
@@ -1,7 +1,7 @@
 import axios from "axios";    
 
 export default axios.create({
-        baseURL: 'https://tinio-myfirstreact.herokuapp.com/api/v1/',
+        baseURL: 'https://tinio-myfirstreact.herokuapp.com/api/v1',
         headers: {
            'Content-Type':'application/json',
            'Access-Control-Allow-Origin': '*'
